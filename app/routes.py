3c580bea6b397039ac3fc56ae206578f21fc3511
Kira <kira.kaur.sidhu@gmail.com>
2023-04-28 19:59:40 -0700

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
+
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
